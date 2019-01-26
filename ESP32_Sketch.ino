32909d5298fe45c04720a4df8dfb551df9c549a9
William Wash <william.wash@sas.com>
2019-01-26 13:57:55 -0500

    Update ESP32_Sketch.ino

--- ESP32_Sketch.ino
+++ ESP32_Sketch.ino
@@ -13,17 +13,22 @@
 
 #define envService BLEUUID((uint16_t)0x181A) 
 BLECharacteristic envHumiCharacteristic(BLEUUID((uint16_t)0x2A6F), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
-BLECharacteristic envTempCharacteristic(BLEUUID((uint16_t)0x2A20), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
+BLECharacteristic envTempCharacteristic(BLEUUID((uint16_t)0x2A6E), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
+//envHumiCharacteristic.addDescriptor(new BLE2902());
+//envTempCharacteristic.addDescriptor(new BLE2902());
 
 #define spo2Service BLEUUID((uint16_t)0x1822) 
 BLECharacteristic spo2PlxCharacteristic(BLEUUID((uint16_t)0x2A5F), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
+//spo2PlxCharacteristic.addDescriptor(new BLE2902());
 
 #define thermoService BLEUUID((uint16_t)0x1809) 
 BLECharacteristic thermoCharacteristic(BLEUUID((uint16_t)0x2A1C), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
+//thermoCharacteristic.addDescriptor(new BLE2902());
 
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
-uint32_t value = 0;
+bool deviceReset = false;
+uint32_t value = 1;
 BLEServer* pServer = NULL;
 
 class MyServerCallbacks: public BLEServerCallbacks {
@@ -49,7 +54,13 @@ long previousMillis = 0;  // last time the sensors were checked, in ms
 void setup() {
   
   Serial.begin(9600);    // initialize serial communication
-  Serial.println("Starting BLE work!");
+  
+  // Set up the OLED screen
+  u8x8.begin();
+  u8x8.setFont(u8x8_font_chroma48medium8_r);
+  u8x8.clearLine(0);
+  u8x8.setCursor(0, 0);
+  u8x8.print("Ready");  
 
   BLEDevice::init("IoTHealth");
   pServer = BLEDevice::createServer();
@@ -82,34 +93,62 @@ void setup() {
 
  
   Serial.println("Waiting a client connection to notify...");
-  
-  // Set up the OLED screen
-  u8x8.begin();
-  u8x8.setFont(u8x8_font_chroma48medium8_r);
-  
-
-  /* Set a local name for the BLE device
-     This name will appear in advertising packets
-     and can be used by remote devices to identify this BLE devicecur
-     The name can be changed but maybe be truncated based on space left in advertisement packet */
-
-
-  Serial.println("Bluetooth device active, waiting for connections...");
-  u8x8.clearLine(0);
-  u8x8.setCursor(0, 0);
-  u8x8.print("Waiting for BLE Device");  
 }
 
 void loop() {
 
-    while (deviceConnected) {
+  if (deviceConnected) {
+      u8x8.setCursor(0, 0);
+      switch (value) {
+        case 1:
+        u8x8.print("Sending |");
+        value +=1;
+        break;
+        case 2:
+        u8x8.print("Sending /");
+        value +=1;
+        break;
+        case 3:
+        u8x8.print("Sending -");
+        value +=1;
+        break;
+        case 4:
+        u8x8.print("Sending \\");
+        value =1;
+        break;
+      }
+    
+  }
+
+  if (!deviceConnected) {
+      u8x8.setCursor(0, 0);
+      switch (value) {
+        case 1:
+        u8x8.print("Waiting |");
+        value +=1;
+        break;
+        case 2:
+        u8x8.print("Waiting /");
+        value +=1;
+        break;
+        case 3:
+        u8x8.print("Waiting -");
+        value +=1;
+        break;
+        case 4:
+        u8x8.print("Waiting \\");
+        value =1;
+        break;
+      }
+  }
+
       long currentMillis = millis();
       // if 200ms have passed, check the heart rate measurement:
       if (currentMillis - previousMillis >= 200) {
         previousMillis = currentMillis;
         updateSensors();
       }
-    }
+
 
   // disconnecting
   if (!deviceConnected && oldDeviceConnected) {
@@ -117,18 +156,12 @@ void loop() {
       pServer->startAdvertising(); // restart advertising
       Serial.println("start advertising");
       oldDeviceConnected = deviceConnected;
-      Serial.print("Disconnected");
-      u8x8.clearLine(0);
-      u8x8.setCursor(0, 0);
-      u8x8.print("Disconnected, waiting for BLE Device"); 
+      Serial.print("Disconnected"); 
   }
   
   // connecting
   if (deviceConnected && !oldDeviceConnected) {
       oldDeviceConnected = deviceConnected;
-      u8x8.clearLine(0);
-      u8x8.setCursor(0, 0);
-      u8x8.print("Connected"); 
   }
 }
 
@@ -152,7 +185,11 @@ void updateSensors() {
   // Pulse Oximeter
   int spo2 = 98;
   int pulse = 89;
-
+  
+  Serial.print("SO2: ");
+  Serial.println(spo2);
+  Serial.print("Pulse: ");
+  Serial.println(pulse);
   u8x8.setCursor(0, 3);
   u8x8.printf("SPO2:  %d%%", spo2);
   u8x8.setCursor(0, 4);
@@ -160,7 +197,8 @@ void updateSensors() {
   
   // Health Thermometer
   int ht = 94;
-  
+  Serial.print("Skin Temp: ");
+  Serial.println(ht);  
   u8x8.setCursor(0, 5);
   u8x8.printf("Skin Temp:  %d%", ht);  
 
@@ -168,13 +206,13 @@ void updateSensors() {
   // Update BLE Characteristics
 
   
-  envHumiCharacteristic.setValue(0, (char)h);
+  envHumiCharacteristic.setValue((uint8_t*)&h,2);
   envHumiCharacteristic.notify();  // and update the environment humidity characteristic
-  envTempCharacteristic.setValue(0, (char)t);
+  envTempCharacteristic.setValue((uint8_t*)&t,2);
   envTempCharacteristic.notify();  // and update the environment humidity characteristic
-  spo2PlxCharacteristic.setValue(0, (char)spo2);
+  spo2PlxCharacteristic.setValue((uint8_t*)&spo2,2);
   spo2PlxCharacteristic.notify();  // and update the environment humidity characteristic
-  thermoCharacteristic.setValue(0, (char)ht);
+  thermoCharacteristic.setValue((uint8_t*)&ht,2);
   thermoCharacteristic.notify();  // and update the environment humidity characteristic
     
 
